30f59467ebf361009a4daa78d3129f0de6eeca5a
Nick Leso <nickleso.work@gmail.com>
2023-08-15 15:43:36 +0300

    Update depMiddleware.js

--- middlewares/depMiddleware.js
+++ middlewares/depMiddleware.js
@@ -8,41 +8,73 @@ const depMiddleware = async (req, res, next) => {
     recieverHromada = null,
   } = req.body;
 
-  const depQuery = `SELECT
+  const depAndOdaEmailQuery = `SELECT
   u.email
   FROM dep_users AS u
-  WHERE u.structureName = ? AND u.position = ?
+  WHERE u.structureName = '${recieverName}' 
+  AND u.position = 'deputy'
   OR
-  u.access = ?
-  AND u.position = ? ;`;
+  u.access = '${recieverLevel}'
+  AND u.position = 'council';`;
 
-  // допистати якщо рівень district, hromada
+  const depAndDistrictEmailQuery = `SELECT
+  u.email
+  FROM dep_users AS u
+  WHERE u.structureName = '${recieverName}' 
+  AND u.position = 'deputy'
+  OR
+  u.access = '${recieverLevel}'
+  AND u.position = 'council'
+  AND u.district = '${recieverDistrict}';`;
+
+  const depAndHromadaEmailQuery = `SELECT
+  u.email
+  FROM dep_users AS u
+  WHERE u.structureName = '${recieverName}' 
+  AND u.position = 'deputy'
+  OR
+  u.access = '${recieverLevel}'
+  AND u.position = 'council'
+  AND u.hromada = '${recieverHromada}';`;
+
+  let emailQuery = "";
+
+  if (recieverLevel === "oda") {
+    emailQuery = depAndOdaEmailQuery;
+  }
+
+  if (recieverLevel === "district" && recieverDistrict && !recieverHromada) {
+    emailQuery = depAndDistrictEmailQuery;
+  }
+
+  if (recieverLevel === "hromada" && recieverHromada) {
+    emailQuery = depAndHromadaEmailQuery;
+  }
 
   try {
-    pool.query(
-      depQuery,
-      [recieverName, "deputy", recieverLevel, "council"],
-      function (err, result, fields) {
-        if (err) {
-          return res.status(404).json({
-            message: err,
-            code: 404,
-          });
-        }
-
-        if (!result.length) {
-          return res.status(404).json({
-            message: "not found",
-            code: 404,
-          });
-        }
-
-        req.dep = {
-          emailList: result.map((item) => item.email),
-        };
-        next();
+    pool.query(emailQuery, function (err, result, fields) {
+      if (err) {
+        return res.status(404).json({
+          message: err,
+          code: 404,
+        });
       }
-    );
+
+      if (!result.length) {
+        return res.status(404).json({
+          message: "not found",
+          code: 404,
+        });
+      }
+
+      const emailList = result.map((item) => item.email);
+      // emailList.push("krupchynskyi.plbl@gmail.com");
+
+      req.dep = {
+        emailList,
+      };
+      next();
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
