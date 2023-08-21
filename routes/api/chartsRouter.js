9a20eb90fbe9bf7bf9261fe28d48876d7c5661ed
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2023-08-21 16:42:26 +0300

    add filter value route

--- routes/api/chartsRouter.js
+++ routes/api/chartsRouter.js
@@ -6,6 +6,7 @@ const { ctrlWrapper, getQueryChartMD } = require("../../middlewares");
 const {
   getPageConfig,
   getChartDataByDynamicQuery,
+  getFilterValues,
 } = require("../../controllers/charts");
 
 chartsRouter.get("/get-page-config/:pageName", ctrlWrapper(getPageConfig));
@@ -14,6 +15,10 @@ chartsRouter.get(
   "/get-chart-data/:id",
   ctrlWrapper(getQueryChartMD),
   ctrlWrapper(getChartDataByDynamicQuery)
+);
+chartsRouter.get(
+  "/get-filter-value/:table/:target",
+  ctrlWrapper(getFilterValues)
 );
 
 module.exports = chartsRouter;
