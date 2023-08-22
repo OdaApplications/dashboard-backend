9022032ca1cd98b3e0ba0c7a2939ca91c90433cf
Nick Leso <nickleso.work@gmail.com>
2023-08-22 10:52:24 +0300

    Update getFilterValues.js

--- controllers/charts/getFilterValues.js
+++ controllers/charts/getFilterValues.js
@@ -1,18 +1,18 @@
+const { pool } = require("../../models/connection");
 const {
   queryFilterFormater,
 } = require("../../middlewares/queryFilterFormater");
-const { pool } = require("../../models/connection");
 
 const getFilterValues = async (req, res, next) => {
   const { target, table } = req.params;
   const { filter } = req.query;
 
-  let query = `SELECT ${target} FROM ${table} filterVar GROUP BY ${target}`;
+  let query = `SELECT ? FROM ? filterVar GROUP BY ?`;
 
   query = queryFilterFormater(query, filter);
 
   try {
-    pool.query(query, function (err, result, fields) {
+    pool.query(query, [target, table, target], function (err, result, fields) {
       if (err) {
         return res.status(404).json({
           message: "not found",
